5d0b156a0bb1d9e5953e91ce45d9fc303437de26
David Gergely <dgergely03@gmail.com>
2023-01-16 23:24:01 +0200

    Configuring the router using standalone features

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {PancakeReceiptsAppComponent} from './app/pancake-receipts-app/pancake-receipts-app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {PancakeReceiptListComponent} from "./app/pancake-receipt-list/pancake-receipt-list.component";
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list'},
+  {path: 'list', component: PancakeReceiptListComponent},
+  {path: 'list/:id', loadComponent: () => import('./app/pancake-receipt-details/pancake-receipt-details.component')}
+]
 
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
 bootstrapApplication(PancakeReceiptsAppComponent,{
-  providers: []
+  providers: [provideRouter(routes)]
 })
   .catch(err => console.error(err));
